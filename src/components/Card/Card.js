fc69f88e494e96735d4d5fe58425fd9ee918d33e
Ines Van Stappen <ines.vanstappen@gmail.com>
2025-08-18 14:06:35 +0200

    Refactor components for consistency: update button styles, enhance `Card` props with support for multiple icons and links, and simplify form validation logic in ToDoListForm for improved readability

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,30 +1,27 @@
-import styles from './Card.module.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {Link} from "react-router-dom";
 
-function Card({width, shadow, title, icon, coloredHeader, link, children}) {
+function Card({width, shadow, title, icons = [], coloredHeader, children}) {
     return <div className={`bg-white rounded-lg ${shadow ? 'shadow-xl' : ''} ${width}`}>
         <h2 className={`relative text-center p-4 ${coloredHeader ? 'coloredBackground' : ''}`}>
             <span>{title}</span>
-            {link ? (
-                <Link to={link}>
-                    {icon && (
-                        <FontAwesomeIcon
-                            icon={icon}
-                            className="absolute right-0 top-1/2 -translate-y-1/2 text-white pr-4"
-                        />
+            {icons.length > 0 && (
+                <div className="absolute right-0 top-1/2 -translate-y-1/2 flex gap-2 pr-4">
+                    {icons.map((icon, index) =>
+                        icon.link ? (
+                            <Link to={icon.link} key={index}>
+                                <FontAwesomeIcon icon={icon.icon} className="text-white"/>
+                            </Link>
+                        ) : (
+                            <FontAwesomeIcon icon={icon.icon} className="text-white" key={index}/>
+                        )
                     )}
-                </Link>
-            ) : (
-                icon && (
-                    <FontAwesomeIcon
-                        icon={icon}
-                        className="absolute right-0 top-1/2 -translate-y-1/2 text-white pr-4"
-                    />
-                )
+                </div>
             )}
         </h2>
-        {children}
+        {
+            children
+        }
     </div>
 }
 
